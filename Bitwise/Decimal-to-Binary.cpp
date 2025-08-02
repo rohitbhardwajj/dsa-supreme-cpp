16b4f9b01ed5e222520719a18d4b98b49da98713
Rohit Bhardwaj <rohitbhardwaj121001@gmail.com>
2025-08-02 17:48:26 +0530

    DOne

--- Bitwise/Decimal-to-Binary.cpp
+++ Bitwise/Decimal-to-Binary.cpp
@@ -19,3 +19,51 @@ int main()
     cout << "Binary: " << binary;
     return 0;
 }
+
+
+
+// approach ---2  using bitset inbuilt function
+
+
+#include <iostream>
+#include <bitset>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Enter a decimal number: ";
+    cin >> n;
+
+    cout << "Binary: " << bitset<32>(n) << endl;  // 32-bit binary
+    return 0;
+}
+
+
+//approach ---3 using stack
+
+#include <iostream>
+#include <stack>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Enter a number: ";
+    cin >> n;
+
+    stack<int> s;
+    if (n == 0) s.push(0);
+
+    while (n > 0) {
+        s.push(n % 2);
+        n /= 2;
+    }
+
+    cout << "Binary: ";
+    while (!s.empty()) {
+        cout << s.top();
+        s.pop();
+    }
+
+    return 0;
+}
+
