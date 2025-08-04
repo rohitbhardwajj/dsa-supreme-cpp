8549fb55f4acd012280fb4ff62c9c0a41db6cb3a
Rohit Bhardwaj <rohitbhardwaj121001@gmail.com>
2025-08-04 19:28:33 +0530

    DOne

--- Bitwise/Power-of-two.cpp
+++ Bitwise/Power-of-two.cpp
@@ -21,3 +21,25 @@ int main()
     }
     return 0;
 }
+
+
+
+// approach---2
+
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+       long long int pow=1;
+
+        if(n==1) return true;
+        while(pow<n){
+            if(2*pow==n){
+                return true;
+                break;
+            }else{
+                pow*=2;
+            }
+        }
+        return false;
+    }
+};
