762a0cda575c2aaab7af15ce7933a2bf8318ca1d
Rohit Bhardwaj <rohitbhardwaj121001@gmail.com>
2025-08-04 10:21:15 +0530

    Done

---
+++ Sorting/Bubble-Sort.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout << "Enter the size of the array: ";
+    cin >> n;
+    int arry[n];
+
+    cout << "Enter elements in the array: ";
+    for (int i = 0; i < n; i++) {
+        cin >> arry[i];
+    }
+
+    cout << "Before Bubble Sort: ";
+    for (int i = 0; i < n; i++) {
+        cout << arry[i] << " ";
+    }
+    cout << endl;
+
+    // Bubble Sort Algorithm
+    for (int i = 0; i < n - 1; i++) {
+        // Last i elements are already in place
+        for (int j = 0; j < n - i - 1; j++) {
+            if (arry[j] > arry[j + 1]) {
+                // Swap adjacent elements if they are in wrong order
+                swap(arry[j], arry[j + 1]);
+            }
+        }
+    }
+
+    cout << "After Bubble Sort: ";
+    for (int i = 0; i < n; i++) {
+        cout << arry[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
