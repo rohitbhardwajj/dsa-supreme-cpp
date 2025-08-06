63e7ee06690e2b7bdd5b438ac1925f05be2cf482
Rohit Bhardwaj <rohitbhardwaj121001@gmail.com>
2025-08-07 01:59:01 +0530

    Done

--- Arrays/Greatest-Freq.cpp
+++ Arrays/Greatest-Freq.cpp
@@ -27,3 +27,39 @@ int main()
 
     return 0;
 }
+
+
+
+// approach-------------2
+
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout << "Enter the size of the array: ";
+    cin >> n;
+
+    int arry[n];
+    cout << "Enter numbers in the array (only one number appears twice): ";
+
+    for (int i = 0; i < n; i++) {
+        cin >> arry[i];
+    }
+
+    for (int i = 0; i < n; i++) {
+        for (int j = i + 1; j < n; j++) {
+            if (arry[i] == arry[j]) {
+                cout << "Number is: " << arry[i] << endl;
+                return 0;  
+            }
+        }
+    
+
+    cout << "No duplicate found." << endl;
+    return 0;
+    }
+}
+
